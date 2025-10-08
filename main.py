fd94ce71fab86782398029e6a53ef2e334d7193d
Marin2907 <marin.ladavac@gmail.com>
2025-10-08 09:52:00 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -13,19 +13,44 @@ def ucitaj_tekst(filepath):
 def ocisti_tekst(tekst):
      #kod za pročišćavanje teksta ide ovdje
     tekst = tekst.lower()
-    return tekst
-if __name__=="__main__":
-        filepath = "tekst.txt"
-        print(f"Učitavam tekst iz datoteke: {filepath}")
-        ucitani_tekst = ucitaj_tekst(filepath)
-        if ucitani_tekst:
-            print("Učitani tekst je:")
-            print(ucitani_tekst)
-        else:
-            print("Greška pri učitavanju datoteke.")
-            ucitani_tekst = ocisti_tekst(ucitani_tekst)
-            if ucitani_tekst:
-                 print("Očišćeni tekst je:")
-                 print(ucitani_tekst)
+    
+    interpunkcija=[`.`,`,`,`!`,`"`,`;`,`:`,"`",`(`,`)`]
+    for znak in interpunkcija:
+        tekst = tekst.replace(znak,``)
+    lista_rijeci = tekst.split()
+    return lista_rijeci
+
+def broji_rijeci(lista_rijeci):
+        #rječnik u koji ćemo spremiti svaku riječ i koliko se puta ta riječ ponovila
+        brojac_riječi = {}
+        for riječ in lista_rijeci:
+            if riječ in brojac_riječi:
+                brojac_riječi[riječ] += 1
             else:
-                 print("Greška pri očišćavanju teksta.")
+                brojac_riječi[riječ] = 1
+        return brojac_riječi
+
+
+if __name__ == "__main__":
+    filepath = "tekst.txt"
+    print(f"učitavam tekst iz datoteke: {filepath}")
+    ucitani_tekst = ucitaj_tekst(filepath)
+    if ucitani_tekst:
+       print("učitani tekst je:")
+       print(ucitani_tekst)
+    else:
+       print("greška pri učitavanju datoteke.")
+
+    ucitani_tekst = ocisti_tekst(ucitani_tekst)     
+
+    if ucitani_tekst:
+       print("ucitani tekst je:")
+       print(ucitani_tekst)
+       #brojanje riječi i ispis rječnika
+       brojac_riječi = broji_rijeci(ucitani_tekst)
+       print("brojac riječi je:")
+       print(brojac_riječi)
+    else:
+       print("greška pri učitavanju teksta.")
+
+
